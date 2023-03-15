16a4a07cb20facdbea6e8f89499691fc4efade8e
Andre Luiz Ferreira Rosa <alfr2@WINDOWS.CIN.UFPE.BR>
2023-03-15 12:13:29 -0300

    ajustando nomes do card para não dar conflito na pagina de editar collections

--- web-app/src/components/ProductCard/index.tsx
+++ web-app/src/components/ProductCard/index.tsx
@@ -14,15 +14,15 @@ export default function ProductCard(props:Prop) : JSX.Element{
     return (
         <article className='ProductCard' {...rest}>
             <div>
-                <img className="Image" alt='imagem do produto' src={product.midias[0].url}/>
-                <span className="Title">{product.title}</span>
+                <img className="CardImage" alt='imagem do produto' src={product.midias[0].url}/>
+                <span className="CardTitle">{product.title}</span>
             </div>
             <div className="Card-infos">
-                <span className="Price">R$ {product.price}</span>
+                <span className="CardPrice">R$ {product.price}</span>
 
                 <div className="RatingSales-wrapper">
-                    <span className="Sales">{product.sales} vendidos</span>
-                    <span className="Rating"><RatingStars rate={product.rating} size="small"/></span>
+                    <span className="CardSales">{product.sales} vendidos</span>
+                    <span className="CardRating"><RatingStars rate={product.rating} size="small"/></span>
                 </div>
             </div>
         </article>
