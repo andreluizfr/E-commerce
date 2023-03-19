dcb8f8c5071580c1f4fc2522557c742eb84c20f5
André <andre.luizfr2@gmail.com>
2023-03-19 15:05:15 -0300

    adicionando animação na abertura do CartPopover, UserDropdownMenu com framer motion. Trocando animação por css por animação do frame motion quando bota produto no carrinho

--- web-app/src/components/NavBar/CartPopover/index.tsx
+++ web-app/src/components/NavBar/CartPopover/index.tsx
@@ -1,6 +1,7 @@
 import './styles.css';
 
 import * as Popover from '@radix-ui/react-popover';
+import { motion, useAnimation } from 'framer-motion';
 
 import shoppingCart from 'assets/svg/shopping-cart.png'; //trocar pra svg
 import addToCart from 'assets/svg/add-to-cart.png'; //trocar pra svg
@@ -15,21 +16,24 @@ import Product from 'types/product';
 
 import { Link } from 'react-router-dom';
 
+
 export default function UserDropdownMenu () :JSX.Element {
     
     const cart = useSelector((state: StoreState) => state.cart);
     const dispatch = useDispatch();
+
+    const cartIconControls = useAnimation();
 
     //sempre que carrinho store atualizar, atualizar o localStorage como backup.
 	useEffect(()=>{
             localStorage.setItem("cart", JSON.stringify(cart.value));
 	}, [cart]);
 
-    function add(product: Product) {
+    function addProductToCart(product: Product) {
         dispatch(addProduct(product));
     }
 
-    function remove(product: Product) {
+    function removeProductFromCart(product: Product) {
         dispatch(removeProduct(product));
     }
 
@@ -37,111 +41,119 @@ export default function UserDropdownMenu () :JSX.Element {
         dispatch(clearCart());
     }
 
-    function animateCart() {
-        const cartIconElement = document.getElementsByClassName("ShoppingCart-icon")[0];
-        cartIconElement.setAttribute("animate", "true");
-
-        setTimeout(()=>{
-            cartIconElement.setAttribute("animate", "false");
-        }, 200); //0.2s igual o tempo da animação
+    function animateCartIcon() {
+        cartIconControls.start({rotate: [45, -45, 45, 0]});
     }
 
     if(cart && cart.value.length > 0)
         return(
             <Popover.Root>
                 <Popover.Trigger className="CartPopoverTrigger">
-                    <img 
+                    <motion.img
                         className='ShoppingCart-icon'
                         alt='icone de carrinho de compras' 
                         src={shoppingCart}
-                    />
+                        animate={cartIconControls}
+                        transition={{type:'spring', bounce: 0.8}}
+                    >
+                    </motion.img>
                 </Popover.Trigger>
 
                 <Popover.Portal>
-                    <Popover.Content className="CartPopoverContent" sideOffset={5}>
-                        {
-                            cart.value.map((productState, index)=>{
-                                return(
-                                    <div className="Item-container" key={index}>
-                                        
-                                        <Link to={'/produto/'+productState.product.productId}>
-                                            <img 
-                                                className="Item-image"
-                                                src={productState.product.midias[0].url} 
-                                                alt="imagem do produto"
-                                            />  
-                                        </Link>
+                    <Popover.Content className="CartPopoverContent" sideOffset={5} asChild forceMount>
+                        <motion.div
+                            initial={{ scale: 0, opacity: 0 }}
+                            animate={{
+                                scale: 1,
+                                opacity: 1,
+                                transition: { type: "spring", duration: 0.3 },
+                            }}    
+                        >
+                            {
+                                cart.value.map((productState, index)=>{
+                                    return(
+                                        <div className="Item-container" key={index}>
+                                            
+                                            <Link to={'/produto/'+productState.product.productId}>
+                                                <img 
+                                                    className="Item-image"
+                                                    src={productState.product.midias[0].url} 
+                                                    alt="imagem do produto"
+                                                />  
+                                            </Link>
 
-                                        <div className="TitleVariation-wrapper">
-                                            <span className='Title'>{productState.product.title}</span>
-                                            <span className='Variation'>
-                                                Variação:
-                                                {   
-                                                    productState.product.variation?
-                                                        Object.keys(productState.product.variation).map(key=>{
-                                                            if(productState.product.variation && productState.product.variation[key])
-                                                                return(" " + productState.product.variation[key] + ", ");
-                                                            else 
-                                                                return "";
-                                                        }).map((variationString, index, variationList)=>{
-                                                            if(index === variationList.length-1)
-                                                                return variationString.split(",")[0];
-                                                            else
-                                                                return variationString;
-                                                        })
-                                                        :
-                                                        null
-                                                }
-                                            </span>
+                                            <div className="TitleVariation-wrapper">
+                                                <span className='Title'>{productState.product.title}</span>
+                                                <span className='Variation'>
+                                                    Variação:
+                                                    {   
+                                                        productState.product.variation?
+                                                            Object.keys(productState.product.variation).map(key=>{
+                                                                if(productState.product.variation && productState.product.variation[key])
+                                                                    return(" " + productState.product.variation[key] + ", ");
+                                                                else 
+                                                                    return "";
+                                                            }).map((variationString, index, variationList)=>{
+                                                                if(index === variationList.length-1)
+                                                                    return variationString.split(",")[0];
+                                                                else
+                                                                    return variationString;
+                                                            })
+                                                            :
+                                                            null
+                                                    }
+                                                </span>
+                                            </div>
+
+                                            <div className='QuantityPrice-wrapper'>
+                                                <span className='Quantity'>Quantidade: {productState.quantity}</span>
+                                                <span className='Price'>R${productState.product.price}</span>
+                                            </div>
+                                            
+                                            <div className='Buttons'>
+                                                <img
+                                                    className='Add-button'
+                                                    src={addToCart} 
+                                                    alt="icone de adicionar no carrinho"
+                                                    onClick={()=>{
+                                                        addProductToCart(productState.product);
+                                                        animateCartIcon();
+                                                    }}
+                                                />
+
+                                                <img
+                                                    className='Remove-button'
+                                                    src={removeFromCart} 
+                                                    alt="icone de adicionar no carrinho"
+                                                    onClick={()=>{
+                                                        removeProductFromCart(productState.product);
+                                                        animateCartIcon();
+                                                    }}
+                                                />
+                                            </div>
+
                                         </div>
+                                    );
+                                })
+                            }
 
-                                        <div className='QuantityPrice-wrapper'>
-                                            <span className='Quantity'>Quantidade: {productState.quantity}</span>
-                                            <span className='Price'>R${productState.product.price}</span>
-                                        </div>
-                                        
-                                        <div className='Buttons'>
-                                            <img
-                                                className='Add-button'
-                                                src={addToCart} 
-                                                alt="icone de adicionar no carrinho"
-                                                onClick={()=>{
-                                                    add(productState.product);
-                                                    animateCart();
-                                                }}
-                                            />
+                            <div className='Bottom-buttons'>    
+                                <button 
+                                    className="Clear-button Button" 
+                                    onClick={()=>{
+                                        clear();
+                                        animateCartIcon();
+                                    }}>
+                                    Limpar carrinho
+                                </button>
+                                <Link to={'/pagamento'}>
+                                    <button className="Finish-button Button">Terminar de comprar</button>
+                                </Link>
+                            </div>
 
-                                            <img
-                                                className='Remove-button'
-                                                src={removeFromCart} 
-                                                alt="icone de adicionar no carrinho"
-                                                onClick={()=>{
-                                                    remove(productState.product);
-                                                    animateCart();
-                                                }}
-                                            />
-                                        </div>
-
-                                    </div>
-                                );
-                            })
-                        }
-
-                        <div className='Bottom-buttons'>    
-                            <button 
-                                className="Clear-button Button" 
-                                onClick={()=>{
-                                    clear();
-                                    animateCart();
-                                }}>
-                                Limpar carrinho
-                            </button>
-                            <Link to={'/pagamento'}>
-                                <button className="Finish-button Button">Terminar de comprar</button>
-                            </Link>
-                        </div>
-
-                        <Popover.Arrow className="CartPopoverArrow" />
+                            <Popover.Arrow className="CartPopoverArrow" />
+                            
+                        </motion.div>
                     </Popover.Content>
                 </Popover.Portal>
             </Popover.Root>
@@ -150,17 +162,31 @@ export default function UserDropdownMenu () :JSX.Element {
         return(
             <Popover.Root>
                 <Popover.Trigger className="CartPopoverTrigger">
-                    <img 
+                    <motion.img
                         className='ShoppingCart-icon'
                         alt='icone de carrinho de compras' 
                         src={shoppingCart}
-                    />
+                        animate={cartIconControls}
+                        transition={{type:'spring', bounce: 0.8}}
+                    >
+                    </motion.img>
                 </Popover.Trigger>
 
                 <Popover.Portal>
-                    <Popover.Content className="CartPopoverContent" sideOffset={5}>
-                        Seu carrinho está vazio...
-                        <Popover.Arrow className="CartPopoverArrow" />
+                    <Popover.Content className="CartPopoverContent" sideOffset={5} asChild forceMount>
+                        <motion.div
+                            initial={{ scale: 0, opacity: 0 }}
+                            animate={{
+                                scale: 1,
+                                opacity: 1,
+                                transition: { type: "spring", duration: 0.3 },
+                            }}    
+                        >
+
+                            Seu carrinho está vazio...
+                            <Popover.Arrow className="CartPopoverArrow" />
+
+                        </motion.div>
                     </Popover.Content>
                 </Popover.Portal>
             </Popover.Root>
--- web-app/src/components/NavBar/UserDropdownMenu/index.tsx
+++ web-app/src/components/NavBar/UserDropdownMenu/index.tsx
@@ -1,6 +1,7 @@
 import './styles.css';
 
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
+import { motion } from "framer-motion";
 
 import userIcon from 'assets/svg/user.png'; //trocar pra svg
 import userOutline from 'assets/svg/user-outline.png';
@@ -11,7 +12,6 @@ import loginIcon from 'assets/svg/login.png';
 import signup from 'assets/svg/signup.png';
 import admin from 'assets/svg/admin.png';
 
-//import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { StoreState } from 'store';
 import { removeUser } from 'store/features/userSlice';
@@ -21,14 +21,13 @@ import { Link } from 'react-router-dom';
 export default function UserDropdownMenu () :JSX.Element {
     
     const user = useSelector((state: StoreState) => state.user);
-    const dispatch = useDispatch();;
+    const dispatch = useDispatch();
 
     function logout(){
         dispatch(removeUser());
         localStorage.removeItem('x-access-token');
         window.location.reload();
     }
-
 
     if(user && user.logged)
         return(
@@ -42,43 +41,52 @@ export default function UserDropdownMenu () :JSX.Element {
                 </DropdownMenu.Trigger>
 
                 <DropdownMenu.Portal>
-                    <DropdownMenu.Content className="UserDropdownMenuContent" sideOffset={5}>
+                    <DropdownMenu.Content className="UserDropdownMenuContent" sideOffset={5} asChild forceMount>
+                            <motion.div
+                                initial={{ scale: 0, opacity: 0 }}
+                                animate={{
+                                    scale: 1,
+                                    opacity: 1,
+                                    transition: { type: "spring", duration: 0.3 },
+                                }}    
+                            >
 
-                        <DropdownMenu.Item className="UserDropdownMenuItem">
-                            <img className="Icon" src={userOutline} alt='icone de usuário' loading="lazy"/>
-                            ver minha conta
-                        </DropdownMenu.Item>
+                                <DropdownMenu.Item className="UserDropdownMenuItem">
+                                    <img className="Icon" src={userOutline} alt='icone de usuário' loading="lazy"/>
+                                    ver minha conta
+                                </DropdownMenu.Item>
 
-                        <DropdownMenu.Separator className="UserDropdownMenuSeparator"/>
+                                <DropdownMenu.Separator className="UserDropdownMenuSeparator"/>
 
-                        <DropdownMenu.Item className="UserDropdownMenuItem">
-                            <img className="Icon" src={packageBox} alt='prancheta de pedidos ao lado de uma caixa' loading="lazy"/>
-                            pedidos
-                        </DropdownMenu.Item>
+                                <DropdownMenu.Item className="UserDropdownMenuItem">
+                                    <img className="Icon" src={packageBox} alt='prancheta de pedidos ao lado de uma caixa' loading="lazy"/>
+                                    pedidos
+                                </DropdownMenu.Item>
 
-                        <DropdownMenu.Item className="UserDropdownMenuItem">
-                            <img className="Icon" src={locationIcon} alt='símbolo de localização' loading="lazy"/>
-                            endereços
-                        </DropdownMenu.Item>
+                                <DropdownMenu.Item className="UserDropdownMenuItem">
+                                    <img className="Icon" src={locationIcon} alt='símbolo de localização' loading="lazy"/>
+                                    endereços
+                                </DropdownMenu.Item>
 
-                        <DropdownMenu.Separator className="UserDropdownMenuSeparator"/>
+                                <DropdownMenu.Separator className="UserDropdownMenuSeparator"/>
 
-                        {user.value?.admin?
-                            <DropdownMenu.Item className="UserDropdownMenuItem">
-                                <img className="Icon" src={admin} alt='usuario com engrenagem' loading="lazy"/>
-                                <Link to='/admin'>admin</Link>
-                            </DropdownMenu.Item>
-                            :
-                            null
-                        }
+                                {user.value?.admin?
+                                    <DropdownMenu.Item className="UserDropdownMenuItem">
+                                        <img className="Icon" src={admin} alt='usuario com engrenagem' loading="lazy"/>
+                                        <Link to='/admin'>admin</Link>
+                                    </DropdownMenu.Item>
+                                    :
+                                    null
+                                }
 
-                        <DropdownMenu.Item className="UserDropdownMenuItem" onClick={logout}>
-                            <img className="Icon" src={logoutIcon} alt='porta pra sair' loading="lazy"/>
-                            sair da conta
-                        </DropdownMenu.Item>
+                                <DropdownMenu.Item className="UserDropdownMenuItem" onClick={logout}>
+                                    <img className="Icon" src={logoutIcon} alt='porta pra sair' loading="lazy"/>
+                                    sair da conta
+                                </DropdownMenu.Item>
 
-                        <DropdownMenu.Arrow className="UserDropdownMenuArrow" />
-
+                                <DropdownMenu.Arrow className="UserDropdownMenuArrow" />
+                            
+                            </motion.div>
                     </DropdownMenu.Content>
                 </DropdownMenu.Portal>
             </DropdownMenu.Root>
@@ -95,22 +103,31 @@ export default function UserDropdownMenu () :JSX.Element {
                 </DropdownMenu.Trigger>
 
                 <DropdownMenu.Portal>
-                    <DropdownMenu.Content className="UserDropdownMenuContent" sideOffset={5}>
+                    <DropdownMenu.Content className="UserDropdownMenuContent" sideOffset={5} asChild forceMount>
+                        <motion.div
+                            initial={{ scale: 0, opacity: 0 }}
+                            animate={{
+                                scale: 1,
+                                opacity: 1,
+                                transition: { type: "spring", duration: 0.3 },
+                            }}    
+                        >
 
-                        <DropdownMenu.Item className="UserDropdownMenuItem">
-                            <img className="Icon" src={loginIcon} alt='porta pra entrar' loading="lazy"/>
-                            <Link to='/login'>fazer login</Link>
-                        </DropdownMenu.Item>
+                            <DropdownMenu.Item className="UserDropdownMenuItem">
+                                <img className="Icon" src={loginIcon} alt='porta pra entrar' loading="lazy"/>
+                                <Link to='/login'>fazer login</Link>
+                            </DropdownMenu.Item>
 
-                        <DropdownMenu.Separator className="UserDropdownMenuSeparator"/>
+                            <DropdownMenu.Separator className="UserDropdownMenuSeparator"/>
 
-                        <DropdownMenu.Item className="UserDropdownMenuItem">
-                            <img className="Icon" src={signup} alt='usuário com símbolo de mais' loading="lazy"/>
-                            <Link to='/cadastro'>cadastrar-se</Link>
-                        </DropdownMenu.Item>
+                            <DropdownMenu.Item className="UserDropdownMenuItem">
+                                <img className="Icon" src={signup} alt='usuário com símbolo de mais' loading="lazy"/>
+                                <Link to='/cadastro'>cadastrar-se</Link>
+                            </DropdownMenu.Item>
 
-                        <DropdownMenu.Arrow className="UserDropdownMenuArrow" />
+                            <DropdownMenu.Arrow className="UserDropdownMenuArrow" />
 
+                        </motion.div>
                     </DropdownMenu.Content>
                 </DropdownMenu.Portal>
             </DropdownMenu.Root>
