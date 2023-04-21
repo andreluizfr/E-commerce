29f96db777c5a25c7ea643117af1e64fd6ea0b8d
André <andre.luizfr2@gmail.com>
2023-04-20 23:58:14 -0300

    inicio da refatoração das queries colocando tratamento de erro nas requests do axios

--- web-app/src/queries/User/logged/GetUser.ts
+++ web-app/src/queries/User/logged/GetUser.ts
@@ -16,11 +16,30 @@ export default function GetUser () {
         
         const accessToken = localStorage.getItem("x-access-token");
 
-        const response = await axios.get('/user/getUser',  {headers: { Authorization: `Bearer ${accessToken}` }});
+        try{
 
-        const data = response.data as IGetUserResponse;
-        
-        return data;
+            const response = await axios.get('/user/getUser',  {headers: { Authorization: `Bearer ${accessToken}` }});
+
+            return response.data as IGetUserResponse;
+
+        } catch (error: any) {
+
+            if (error.response) {
+                // The request was made and the server responded with a status code that falls out of the range of 2xx 
+                console.log(error.response.status);
+                return error.response.data as IGetUserResponse;
+            }
+            else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the browser 
+                console.log('O servidor não pode responder a essa requisição.');
+            } 
+            else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('Error:', error.message);
+            }
+
+        }
 
     }, {
         refetchOnWindowFocus: false,
